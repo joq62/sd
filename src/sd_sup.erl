0a1506b2d69a417a55ffa2d2d84cce6ef88fb719
joq62 <jakim.leche@gmail.com>
2023-01-21 06:16:11 +0100

    corr

--- src/sd_sup.erl
+++ src/sd_sup.erl
@@ -29,7 +29,9 @@ init([]) ->
     SupFlags = #{strategy => one_for_all,
                  intensity => 0,
                  period => 1},
-    ChildSpecs = [],
+    ChildSpecs = [
+		  #{id=>sd,
+		     start=>{sd,start,[]}}],
     {ok, {SupFlags, ChildSpecs}}.
 
 %% internal functions
